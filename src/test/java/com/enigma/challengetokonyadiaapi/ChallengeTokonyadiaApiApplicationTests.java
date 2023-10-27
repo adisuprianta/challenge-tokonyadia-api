6e719a9b1cad938bd7eec7e46822fb938e06ae78
adisuprianta <adisuprianta13@gmail.com>
2023-10-27 22:58:12 +0700

    initial commit

---
+++ src/test/java/com/enigma/challengetokonyadiaapi/ChallengeTokonyadiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.challengetokonyadiaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTokonyadiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
